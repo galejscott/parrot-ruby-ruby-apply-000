3cde2ca90c11b353cec836ccf0172fe1e31004ce
Scott Gale <galejscott@gmail.com>
2019-04-12 13:38:37 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-parrot("Squawk!")
-parrot("Pretty Bird!")
 
 def parrot(phrase="Squawk!")
   puts "#{phrase}"
   "#{phrase}"
 end
+parrot("Squawk!")
+parrot("Pretty Bird!")
